1c6af8f34da88835b1ce0e56a014599be82f17a0
Abdullah Nasir <145276051+itsmalikabdullahnasir@users.noreply.github.com>
2024-03-14 22:29:16 +0500

    Add files via upload

---
+++ LAB-4/TASK1.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int value) : data(value), next(nullptr) {}
+};
+
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    void insert(int value) {
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    bool search(int value) {
+        Node* temp = head;
+        while (temp != nullptr) {
+            if (temp->data == value)
+                return true;
+            temp = temp->next;
+        }
+        return false;
+    }
+
+    void reverse() {
+        Node* prev = nullptr;
+        Node* curr = head;
+        Node* next = nullptr;
+
+        while (curr != nullptr) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    void print() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+
+    list.insert(3);
+    list.insert(2);
+    list.insert(1);
+
+    std::cout << "Original List: ";
+    list.print();
+
+    std::cout << "Searching for 2: " << list.search(2) << std::endl;
+    std::cout << "Searching for 4: " << list.search(4) << std::endl;
+
+    list.reverse();
+    std::cout << "Reversed List: ";
+    list.print();
+
+    return 0;
+}
